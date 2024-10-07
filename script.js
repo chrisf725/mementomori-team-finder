b800ebbff9f18588fdf3d32e6aa24eb884f1f65f
chrisf725 <chfrancisco725@yahoo.com>
2024-10-07 11:05:41 -0700

    Fix bug pertaining to no equip chars in team pos no.1

--- script.js
+++ script.js
@@ -1,101 +1,3 @@
-// const characterNames = {
-//     1: 'Monica',
-//     2: 'Illya',
-//     3: 'Iris',
-//     4: 'Loki',
-//     5: 'Soltina',
-//     6: 'Amleth',
-//     7: 'Fenrir',
-//     8: 'Florence',
-//     9: 'Sonya',
-//     10: 'Moddey',
-//     11: 'Charlotte',
-//     12: 'Arianrhod',
-//     13: 'Theodora',
-//     14: 'Petra',
-//     15: 'Sabrina',
-//     16: 'Freesia',
-//     17: 'Amour',
-//     18: 'Rean',
-//     19: 'Belle',
-//     20: 'Dian',
-//     21: 'Shizu',
-//     22: 'Zara',
-//     23: 'Rosalie',
-//     24: 'Libra',
-//     25: 'Ivy',
-//     26: 'Merlyn',
-//     27: 'Cordie',
-//     28: 'Nina',
-//     29: 'Mertillier',
-//     30: 'Luke',
-//     31: 'Garmr',
-//     32: 'Skuld',
-//     33: 'Cherna',
-//     34: 'Soteira',
-//     35: 'Mimi',
-//     36: 'Tropon',
-//     37: 'Hathor',
-//     38: 'Olivia',
-//     39: 'Primavera',
-//     40: 'Carol',
-//     41: '[The Witch of Mourning Flowers] Natasha',
-//     42: '[The Witch of Sacred Swords] Fortina',
-//     43: '[The Witch of Wailing Lightning] Cerberus',
-//     44: '[The Witch of Torrential Sorrow] Rusalka',
-//     45: '[The Witch of Longinus] Elfriede',
-//     46: '[The Witch of Snowy Illusions] Lunalynn',
-//     47: '[The Witch of Conflagration] Valeriede',
-//     48: '[The Witch of Rust] A.A.',
-//     49: '[The Witch of Fallen Crystals] Ophelia',
-//     50: '[The Witch of Lost Souls] Armstrong',
-//     51: 'Sophia',
-//     52: 'Sivi',
-//     53: 'Veela',
-//     54: 'Chiffon',
-//     55: 'Lea',
-//     56: 'Claudia',
-//     57: 'Stella',
-//     58: 'Artie',
-//     59: 'Eir',
-//     60: 'Fia',
-//     61: '[The Witch of God\'s Curse] Illya',
-//     62: 'Priscilla',
-//     63: '[The Witch of Justice] Paladea',
-//     64: 'Gil\'uial',
-//     65: 'Aine',
-//     66: '[Attendant Tainted in Black] Iris',
-//     67: 'Richesse',
-//     68: 'Fenny',
-//     69: '[The Witch of Dreams] Kaguya',
-//     70: '[Soldier of the Summer Breeze] Sabrina',
-//     71: '[Gravekeeper\'s Summer Holiday] Moddey',
-//     72: '[Summer\'s Reverb] Cordie',
-//     73: '[Holy Night\'s Gift] Amour',
-//     74: '[Holy Night\'s Prayer] Tropon',
-//     75: 'Morgana',
-//     76: 'Yuni',
-//     77: 'Minasumari',
-//     78: 'Asahi',
-//     79: 'Serruria',
-//     80: 'Milla',
-//     81: 'Tama',
-//     82: 'Alexandra',
-//     83: '',
-//     84: 'Liselotte',
-//     85: 'Matilda',
-//     86: '',
-//     87: '',
-//     88: '[Apostle of Caritas] Rosalie',
-//     89: '',
-//     90: '',
-//     91: '',
-//     92: '',
-//     93: '',
-//     94: '',
-//     95: '[Little Big Adventure] Nina',
-//     96: '[A Midsummer Day\'s Dream] Amleth'
-// };
 const generateImageURL = (id) => {
     const paddedId = String(id).padStart(6, '0');
     return `https://raw.githubusercontent.com/ScobraCK/MementoMori-data/main/Assets/Characters/Sprites/CHR_${paddedId}_00_s.png`;
@@ -368,36 +270,8 @@ document.getElementById('getTeam').addEventListener('click', async () => {
             }
             return response.json();
         }));
-        // if (!response.ok) {
-        //     throw new Error(`HTTP error, status = ${response.status}`);
-        // }
-        // const data = await response.json();
 
         console.log(data);
-
-        // console.log(data.data[10].PlayerName);
-        // console.log(data.data[10].UserCharacterInfoList[0].CharacterId);
-
-        // // Iterate over the UserCharacterInfoList array and log each element
-        // // Grabs the team's character IDs of the player
-        // data.data[10].UserCharacterInfoList.forEach ((characterInfo, index) => {
-        //     const characterName = characterNames[characterInfo.CharacterId] || 'Unknown Character';
-        //     console.log(`Character ${index + 1}:`, characterName);
-        // });
-
-        // // Find a specific character ID in the team
-        // const targetCharacterId = 6;
-        // data.data.forEach(player => {
-        //     player.UserCharacterInfoList.forEach(characterInfo => {
-        //         if (characterInfo.CharacterId === targetCharacterId) {
-        //             console.log(player.PlayerName);
-        //             player.UserCharacterInfoList.forEach(info => {
-        //                 const characterName = characterNames[info.CharacterId] || 'Unknown Character';
-        //                 console.log(`Character ID: ${info.CharacterId}, Name: ${characterName}`);
-        //             });
-        //         }
-        //     });
-        // });
 
         const resultsTable = document.getElementById('resultsTable');
         resultsTable.innerHTML = ''; // Clear the table
@@ -421,15 +295,23 @@ document.getElementById('getTeam').addEventListener('click', async () => {
         data.forEach(apiData => {
             apiData.data.forEach(player => {
                 /*
-                    Unique identifier found through character equipment
+                    Unique identifier found through character equipment.
+                    There are edge cases where the player has no equipment on their characters, therefore
+                    those players will not be included in the results.
                 */
-                if (!uniquePlayers.has(player.UserCharacterInfoList[0].UserEquipmentDtoInfos[0]?.PlayerId)) {
-                    uniquePlayers.add(player.UserCharacterInfoList[0].UserEquipmentDtoInfos[0]?.PlayerId);
+                let playerId = null
+                for (let i = 0; i < 5 && !playerId; i++) {
+                    for (let j = 0; j < 6 && !playerId; j++) {
+                        playerId = player.UserCharacterInfoList[i]?.UserEquipmentDtoInfos[j]?.PlayerId;
+                    }
+                }
+                if (playerId && !uniquePlayers.has(playerId)) {
+                    uniquePlayers.add(playerId);
 
                     const characterIds = player.UserCharacterInfoList.map(characterInfo => characterInfo.CharacterId);
                     if (selectedCharacterIds.every(id => characterIds.includes(id))) {
                         console.log(player.PlayerName);
-                        console.log(player.UserCharacterInfoList[0].UserEquipmentDtoInfos[0].PlayerId);
+                        console.log(playerId);
 
                         const tr = document.createElement('tr');
 
@@ -442,6 +324,11 @@ document.getElementById('getTeam').addEventListener('click', async () => {
                             const characterName = characterNames[info.CharacterId]?.name || 'Unknown Character';
                             const characterRarity = rarity[info.RarityFlags] || 'Unknown Rarity';
                             console.log(`Character ID: ${info.CharacterId}, Name: ${characterName} Lv. ${info.Level} (${characterRarity})`);
+
+                            // // Debug
+                            // if (characterName === 'Soltina') {
+                            //     console.log(info.UserEquipmentDtoInfos);
+                            // }
 
                             const characterDiv = document.createElement('div');
                             characterDiv.className = 'character-container'
@@ -476,18 +363,6 @@ document.getElementById('getTeam').addEventListener('click', async () => {
         table.appendChild(thead);
         table.appendChild(tbody);
         resultsTable.appendChild(table);
-
-        // data.data.forEach(player => {
-        //     const characterIds = player.UserCharacterInfoList.map(characterInfo => characterInfo.CharacterId);
-        //     if (selectedCharacterIds.every(id => characterIds.includes(id))) {
-        //         console.log(player.PlayerName);
-        //         player.UserCharacterInfoList.forEach(info => {
-        //             const characterName = characterNames[info.CharacterId] || 'Unknown Character';
-        //             const characterRarity = rarity[info.RarityFlags] || 'Unknown Rarity';
-        //             console.log(`Character ID: ${info.CharacterId}, Name: ${characterName} Lv. ${info.Level} (${characterRarity})`);
-        //         });
-        //     }
-        // });
 
     } catch (error) {
         console.error('Error fetching team:', error);
